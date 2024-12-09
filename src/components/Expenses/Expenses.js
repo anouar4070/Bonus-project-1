aaa74272bf839f44f59bd0ebeb704a71a778146c
anouar garbaa <g.anouar@yahoo.com>
2024-12-09 18:05:32 +0100

    outputting conditional message

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -24,6 +24,22 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -33,14 +49,8 @@ const Expenses = (props) => {
         />
         {/* <p>Data for years {filterInfoText} is hidden.</p> */}
 
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {/* {props.items.map((expense) => ( */}
+        {expensesContent}
 
         {/* <ExpenseItem
           title={props.items[0].title}
